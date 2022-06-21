a1aded826c58d44b6a2506383d807745cbbaabe6
Fitrie <mfitrie78@gmail.com>
2022-06-21 11:17:13 +0800

    Initial comit

---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button text="Add Task" color="green"/>
+        <!-- <Button text="Update Taskk" color="blue"/>
+        <Button text="Delete Task" color="red"/> -->
+    </header>
+</template>
+
+
+<script>
+import Button from './Button.vue'
+
+    export default {
+        name: 'Header',
+        // props: ['title'],
+        // props: {
+        //     title: {
+        //         type: String,
+        //         default: 'Hello World'
+        //     }
+        // },
+        props: {
+            title: String
+        },
+        components: {
+            Button
+        }
+    }
+</script>
+
+
+<!--style scope is only for this header-->
+<style scoped>
+    header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
---
+++ src/components/Task.vue
@@ -0,0 +1,54 @@
+<template>
+    <!-- <div class="task">
+        <h3>{{task.text}}
+            <i class="fas fa-times"></i>
+        </h3>
+        <p>{{task.day}}</p>
+    </div> -->
+
+    <div :class="[task.reminder ? 'reminder' : '', 'task']">
+        <h3>{{task.text}}
+            <i @click="onDelete(task.id)" class="fas fa-times"></i>
+        </h3>
+        <p>{{task.day}}</p>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'Task',
+        props:{
+            task: Object,
+        },
+        methods:{
+            // onDelete(id){
+            //     console.log(id);
+            // }
+            onDelete(id){
+                this.$emit('delete-task', id)
+            }
+        }
+    }
+</script>
+
+
+<style scope>
+    .fas {
+    color: red;
+    }
+    .task {
+    background: #f4f4f4;
+    margin: 5px;
+    padding: 10px 20px;
+    cursor: pointer;
+    }
+    .task.reminder {
+    border-left: 5px solid green;
+    }
+    .task h3 {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    }
+</style>
